6deec4e1c19ac20fbeaffaad785f8413a03f837a
Cody Plante <codyrplante@gmail.com>
2013-08-27 21:25:00 -0700

    use of shared preference for user creds

--- src/com/plantec/uwm/LoginActivity.java
+++ src/com/plantec/uwm/LoginActivity.java
@@ -10,6 +10,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.EditText;
 
@@ -38,8 +39,7 @@ public class LoginActivity extends Activity {
 	public void login(View view){
 		mUsername = mUsernameField.getText().toString();
 		mPassword = mPasswordField.getText().toString();
-//		mUsername = "cplante";
-//		mPassword = "Dragoon1";
+		
 		try {
 			if (!isNetworkAvailable()){
 				mUsernameField.setText("Network Bro");
@@ -49,9 +49,14 @@ public class LoginActivity extends Activity {
 				mUsernameField.setText("Wrong Creds Bro");
 				throw new Exception("Wrong creds");
 			}
+			
+			SharedPreferences settings = getSharedPreferences("UserInfo", 0);
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putString("Username", mUsername);
+			editor.putString("Password", mPassword);
+			editor.commit();
+			
 			Intent intent = new Intent(this, MainActivity.class);
-			intent.putExtra("username", mUsername);
-			intent.putExtra("password", mPassword);
 			startActivity(intent);
 		} catch (Exception e) {
 			//mUsernameField.setText("Wrong Creds");
--- src/com/plantec/uwm/MainActivity.java
+++ src/com/plantec/uwm/MainActivity.java
@@ -1,35 +1,52 @@
 package com.plantec.uwm;
 
 import org.jsoup.nodes.Document;
-
 import com.plantec.uwm.http.HttpManager;
 import com.plantec.uwm.mail.MailHandler;
-
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private SharedPreferences settings;
 	private HttpManager mHttp;
 	private TextView mText;
 	private ListView mList;
 	private String mUsername;
 	private String mPassword;
 	
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		settings = getSharedPreferences("UserInfo", 0);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
 		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+		
+		if (!settings.contains("Username")){
+			Intent intent = new Intent(this, LoginActivity.class);
+			startActivity(intent);
+		} else {
 		mText = (TextView) findViewById(R.id.text);
 		mList = (ListView) findViewById(R.id.main_list);
 		mHttp = HttpManager.getInstance();
 		
-		Intent intent = getIntent();
-		mUsername = intent.getExtras().get("username").toString();
-		mPassword = intent.getExtras().get("password").toString();
+		mUsername = settings.getString("Username", "").toString();
+		mPassword = settings.getString("Password", "").toString();
+
+		try {
+			mHttp.login(mUsername, mPassword);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		int result = 0;
 		try {
@@ -48,6 +65,10 @@ public class MainActivity extends Activity {
         
         mList.setAdapter(adapter);
 		
-		mText.setText(Integer.toString(result));
+		mText.setText(result + " " + mUsername);
+//		SharedPreferences.Editor editor = settings.edit();
+//		editor.clear();
+//		editor.commit();
+		}
 	}
 }
